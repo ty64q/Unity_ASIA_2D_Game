b9ddc476ddec281b53c96a91f96ecb488f811e98
ty64q <71687806+ty64q@users.noreply.github.com>
2020-10-27 16:00:05 +0800

    1027

---
+++ 2DGame/Assets/素材/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    //註解：說明
+    //按鈕如何跟程式溝通
+    //需要：方法(函式、功能) - 一堆有功能的程式，演算法
+    //語法：
+    //類型 方法名稱() {}
+    //void 無類型
+void StartGame()
+    {
+
+    }
+    void QuitGame()
+    {
+
+    }
+}
